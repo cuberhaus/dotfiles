797dc3e860b852b2a79a9dfe0b0cc07ecf401bd8
polthelost <polcg@hotmail.es>
2020-07-15 21:21:51 +0200

    added ycm

---
+++ scripts/ycm.sh
@@ -0,0 +1,14 @@
+arch_based_install () {
+    sudo pacman -S patchelf go npm mono python python3 cmake llvm
+}
+ycm_install () {
+    mkdir ycm_build
+    cd ycm_build
+    cmake -G "Unix Makefiles" . ~/.vim/plugged/YouCompleteMe/third_party/ycmd/cpp
+    cmake --build . --target ycm_core --config Release
+    cd ~/.vim/plugged/YouCompleteMe
+    python3 install.py --all
+    rm -r ycm_build
+}
+cd ~
+ycm_install
