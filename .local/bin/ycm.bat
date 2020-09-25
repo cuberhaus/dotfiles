b213b8ed4b14d7489d91543420675adf5a65aa79
Pol Casacuberta Gil <polcg@hotmail.es>
2020-09-26 01:10:45 +0200

    yolo

--- .local/bin/ycm.bat
+++ .local/bin/ycm.bat
@@ -1,5 +1,13 @@
 windows_install () {
     choco install visualstudio2017buildtools
     choco install golang
-    choco install nodejs
+    choco install nodejs Rem npm
+    choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System' Rem cmake
+    Rem vim plug neovim
+    iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    ni "$env:LOCALAPPDATA/nvim-data/site/autoload/plug.vim" -Force
+    Rem npm
+    npm install -g neovim
+    Rem Python3
+    pip install --upgrade neovim
 }
