a87fd6bb992d8f6e52f022143a7591d4c8e92f68
cuberhaus <pol@gmail.com>
2020-09-26 21:16:59 +0200

    idk

---
+++ .config/cinnamon/load.sh
@@ -0,0 +1,4 @@
+dconf load /org/cinnamon/ < ~/.config/cinnamon/cinnamon.dconf
+dconf load /org/nemo/ < ~/.config/cinnamon/nemo.dconf
+dconf load /org/gtk/ < ~/.config/cinnamon/gtk.dconf
+dconf load /org/gnome/ < ~/.config/cinnamon/gnome.dconf
