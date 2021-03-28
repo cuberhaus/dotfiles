adadbdab42a28a96038d0b6cd85366da4075ee8f
pol <pol@hotmail.es>
2021-03-29 00:29:16 +0200

    icons added and home work and spotify workspaces changed to 1 2 and 3 easier to remember

--- .config/i3-layout-manager/layouts/spotify.sh
+++ .config/i3-layout-manager/layouts/spotify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-i3-msg "workspace "3: Spotify"; append_layout /home/pol/.config/i3-layout-manager/layouts/layout-SPOTI.json"
+i3-msg "workspace "3: 3"; append_layout /home/pol/.config/i3-layout-manager/layouts/layout-SPOTI.json"
 
 (termite -c ~/.config/termite/cava -e cava &)
 (spotify &)
