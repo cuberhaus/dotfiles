1e1c44b743d14bebc1d466ab415e3dceadd230ad
cuberhaus <polcg@hotmail.es>
2022-04-23 00:03:00 +0200

    building emacs config, added themes and more good stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,7 +8,7 @@
 ;(menu-bar-mode -1)
 (set-face-attribute 'default nil :font "SauceCodePro Nerd Font 11")
 ;(setq visible-bell t)
-;(load-theme 'tango-dark)
+(load-theme 'doom-one)
 
 ;; Initialize package sources
 (require 'package) ; bring in package module
@@ -26,7 +26,8 @@
    (package-install 'use-package))
 
 (require 'use-package)
-(setq use-package-always-ensure t)
+(setq use-package-always-ensure t) ;; equivalent to writing :ensure t in all packages
+;; makes sure that package is downloaded before use
 (setq x-select-enable-clipboard-manager nil); weird emacs bug where it won't close
 ;(use-package command-log-mode)
 
@@ -57,6 +58,8 @@
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
 
+(use-package doom-themes) ;; counsel-load-theme to load a theme from the list
+
 (column-number-mode)
 (global-display-line-numbers-mode t) ;; display line numbers everywhere
 
@@ -75,14 +78,40 @@
   :diminish which-key-mode
   :config ;; this is run after the package is loaded
   (setq which-key-idle-delay 0.15)) ;; delay on keybindings 
+
 ;; eval last sexp is better cause inconsistencies from hooks when running evalbuffer
-;;; AUTOMATED CODE
+;; and show keybindings
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+;; With ivy-rich shows descriptions for commands 
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	  ("C-x b" . counsel-ibuffer)
+	  ("C-x C-f" . counsel-find-file)
+	  :map minibuffer-local-map
+	  ("C-r" . 'counsel-minibuffer-history))
+	 :config
+	 (setq ivy-initial-inputs-alist nil))
+
+(use-package helpful ;; better function descriptions
+  :custom ;; custom variables
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function) ;; remap keybinding to something different
+  ([remap describe-command] . helpful-command) 
+  ([remap describe-variable] . counsel-describe-variable))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(which-key rainbow-delimiters doom-modeline use-package)))
+ '(custom-safe-themes
+   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
+ '(package-selected-packages
+   '(doom-themes which-key use-package rainbow-delimiters ivy-rich helpful doom-modeline counsel command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
