b5400e22708c76649e25c43e2df13d6cac87d0aa
cuberhaus <polcg@hotmail.es>
2022-04-23 11:20:43 +0200

    added emacs evil tpope commentary :D

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,5 @@
 ;; prefixes: C-x, C-h, M-x
+;; emacs swap window C-x o
 (setq inhibit-startup-message t) ; Disable startup menu
 
 (scroll-bar-mode -1) ; Disable the scrollbar
@@ -8,7 +9,7 @@
 ;(menu-bar-mode -1)
 (set-face-attribute 'default nil :font "SauceCodePro Nerd Font 11")
 ;(setq visible-bell t)
-(load-theme 'doom-one)
+(load-theme 'doom-one t) ;; if not using t will prompt if its safe to https://github.com/Malabarba/smart-mode-line/issues/100
 
 ;; Initialize package sources
 (require 'package) ; bring in package module
@@ -36,7 +37,7 @@
 
 (use-package ivy ; makes navigation between stuff easier
   :diminish ; do not show stuff on bar or something
-  :bind (("C-s" . swiper)
+  :bind (("C-s" . swiper) ;;like / but with context
          :map ivy-minibuffer-map
          ("TAB" . ivy-alt-done)	
          ("C-l" . ivy-alt-done)
@@ -52,6 +53,10 @@
   :config
   (ivy-mode 1))
 
+(global-set-key (kbd "C-M-j") 'counsel-switch-buffer) ;; easier command to switch buffers
+;; example (define-key emacs-lisp-mode-map (kbd "C-x M-t") 'counsel-load-theme) define keybinding only in emacs-lisp-mode
+
+(use-package all-the-icons)
 ;; custom command line
 (use-package doom-modeline
   :ensure t
@@ -103,6 +108,70 @@
   ([remap describe-function] . counsel-describe-function) ;; remap keybinding to something different
   ([remap describe-command] . helpful-command) 
   ([remap describe-variable] . counsel-describe-variable))
+
+(use-package general ;; set personal bindings for leader key for example
+ ; (general-define-key "C-M-j" 'counsel-switch-buffer) ;; allows to define multiple global keybindings
+  :config
+  (general-create-definer rune/leader-keys
+  :keymaps '(normal insert visual emacs)
+  :prefix "SPC" 
+  :global-prefix "C-SPC") ;; leader
+  (rune/leader-keys
+   "t" '(:ignore t :which-key "toggles") ;; "folder" for toggles
+   "w" '(save-buffer :which-key "save buffer") ;; classic vim save
+   "tt" '(counsel-load-theme :which-key "choose theme")))
+
+;; vim keybindings for easier on the fingers typing :D
+;; C-r WONT WORK, cause we are not using a redo we have a undo stack
+;; undo is treated like a normal command, therefore you can undo the undo.
+;; undo's will go to the stack, if a move like "k" is done then you can undo again, this makes it possible to undo every single edit.
+;; in typicall (global-undo-tree-mode) (evil-set-undo-system 'undo-tree)
+;; if you are doing several undos and miss the "correct spot" and do anything at all which is not an undo command, you are stuck: you broke the chain of undos https://www.emacswiki.org/emacs/RedoMode
+
+(use-package evil
+  :init
+  (setq evil-want-integration t) ;; must have
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  ;(define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+;; https://github.com/linktohack/evil-commentary
+;; use-package makes it so that it installs it from config and config section
+;; activates the mode
+(use-package evil-commentary
+  :config
+  (evil-commentary-mode))
+
+(use-package evil-collection
+  :after evil ;; load after evil, must have
+  :config
+  (evil-collection-init))
+
+; C-z go back to EMACS MODE
+;;  if package give error try:type list-packages command to update packages
+ 
+(use-package hydra) ;; emacs bindings that stick around like mode for i3
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(rune/leader-keys
+  "ts" '(hydra-text-scale/body :which-key "scale text"))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -111,7 +180,7 @@
  '(custom-safe-themes
    '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
  '(package-selected-packages
-   '(doom-themes which-key use-package rainbow-delimiters ivy-rich helpful doom-modeline counsel command-log-mode)))
+   '(evil-commentary evil-commentary-mode hydra evil-collection evil general doom-themes which-key use-package rainbow-delimiters ivy-rich helpful doom-modeline counsel command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
