57f011de897f2147fa9848a7321d9d370d693eba
cuberhaus <polcg@hotmail.es>
2022-04-24 17:29:26 +0200

    MORE ORG

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,7 @@
 ;; prefixes: C-x: emacs keybindings, C-h: help, M-x: execute function, C-c: user-defined keybindings, C-SPC: more user defined keybindings
 ;; emacs go between windows C-x o
 ;; use bookmarks to open specific files
+;; to get help on variable of function put corsor over it and press C-h v or C-h f
 
 ;; UNDO 
 ;; C-r WONT WORK, cause we are not using a redo we have a undo stack
@@ -74,9 +75,9 @@
 ;; (use-package langtool)
 
 ;; has to install pdf2svg on pc first
-(use-package org-inline-pdf
-  :init
-  (add-hook 'org-mode-hook #'org-inline-pdf-mode))
+;; (use-package org-inline-pdf
+;;   :init
+;;   (add-hook 'org-mode-hook #'org-inline-pdf-mode))
 
 
 (use-package ivy ; makes navigation between stuff easier
@@ -178,6 +179,7 @@
    "t" '(:ignore t :which-key "toggles") ;; "folder" for toggles
    "b" '(:ignore b :which-key "buffers") 
    "h" '(:ignore h :which-key "git-gutter") 
+   "c" '(org-capture :which-key "org-capture") ;; this is F*** awesome
    "g" '(git-gutter-mode :which-key "git-gutter toggle") 
    "hn" '(git-gutter:next-hunk :which-key "next hunk") 
    "hp" '(git-gutter:previous-hunk :which-key "previous hunk") 
@@ -325,6 +327,118 @@
   (setq org-ellipsis " ▾") ;; change ... to another symbol that is less confusing
   (efs/org-font-setup) ;; setup font
    ;; hides *bold* and __underlined__ and linked words [name][link]
+  (setq org-agenda-start-with-log-mode t)
+  (setq org-log-done 'time) ;; logs when a task goes to done C-h-v (describe variable)
+  (setq org-log-into-drawer t) ;; collapse logs into a drawer
+  (setq org-agenda-files
+	'("~/fib/org/birthday.org"
+	  "~/fib/org/Tasks.org"
+	  "~/fib/org/Habits.org"
+	  ))
+
+  (require 'org-habit)
+  (add-to-list 'org-modules 'org-habit) ;;  add org-habit to org-modules
+  (setq org-habit-graph-column 60) ;; what column the habit tracker shows
+
+  (setq org-todo-keywords
+    '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
+      (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
+
+  (setq org-refile-targets ;; move TODO tasks to a different file
+    '(("Archive.org" :maxlevel . 1)
+      ("Tasks.org" :maxlevel . 1)))
+
+  ;; Save Org buffers after refiling!
+  (advice-add 'org-refile :after 'org-save-all-org-buffers)
+
+  (setq org-tag-alist
+    '((:startgroup)
+       ; Put mutually exclusive tags here
+       (:endgroup)
+       ("@errand" . ?E)
+       ("@home" . ?H)
+       ("@work" . ?W)
+       ("agenda" . ?a)
+       ("planning" . ?p)
+       ("publish" . ?P)
+       ("batch" . ?b)
+       ("note" . ?n)
+       ("idea" . ?i)))
+
+;; Configure custom agenda views
+  (setq org-agenda-custom-commands
+   '(("d" "Dashboard"
+     ((agenda "" ((org-deadline-warning-days 7)))
+      (todo "NEXT"
+        ((org-agenda-overriding-header "Next Tasks")))
+      (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
+
+    ("n" "Next Tasks"
+     ((todo "NEXT"
+        ((org-agenda-overriding-header "Next Tasks")))))
+
+    ("W" "Work Tasks" tags-todo "+work-email")
+
+    ;; Low-effort next actions
+    ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
+     ((org-agenda-overriding-header "Low Effort Tasks")
+      (org-agenda-max-todos 20)
+      (org-agenda-files org-agenda-files)))
+
+    ("w" "Workflow Status"
+     ((todo "WAIT"
+            ((org-agenda-overriding-header "Waiting on External")
+             (org-agenda-files org-agenda-files)))
+      (todo "REVIEW"
+            ((org-agenda-overriding-header "In Review")
+             (org-agenda-files org-agenda-files)))
+      (todo "PLAN"
+            ((org-agenda-overriding-header "In Planning")
+             (org-agenda-todo-list-sublevels nil)
+             (org-agenda-files org-agenda-files)))
+      (todo "BACKLOG"
+            ((org-agenda-overriding-header "Project Backlog")
+             (org-agenda-todo-list-sublevels nil)
+             (org-agenda-files org-agenda-files)))
+      (todo "READY"
+            ((org-agenda-overriding-header "Ready for Work")
+             (org-agenda-files org-agenda-files)))
+      (todo "ACTIVE"
+            ((org-agenda-overriding-header "Active Projects")
+             (org-agenda-files org-agenda-files)))
+      (todo "COMPLETED"
+            ((org-agenda-overriding-header "Completed Projects")
+             (org-agenda-files org-agenda-files)))
+      (todo "CANC"
+            ((org-agenda-overriding-header "Cancelled Projects")
+             (org-agenda-files org-agenda-files)))))))
+
+ (setq org-capture-templates
+    `(("t" "Tasks / Projects")
+      ("tt" "Task" entry (file+olp "~/fib/org/Tasks.org" "Inbox")
+           "* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)
+
+      ("j" "Journal Entries")
+      ("jj" "Journal" entry
+           (file+olp+datetree "~/fib/org/Journal.org")
+           "\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
+           ;; ,(dw/read-file-as-string "~/Notes/Templates/Daily.org")
+           :clock-in :clock-resume
+           :empty-lines 1)
+      ("jm" "Meeting" entry
+           (file+olp+datetree "~/fib/org/Journal.org")
+           "* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
+           :clock-in :clock-resume
+           :empty-lines 1)
+
+      ("w" "Workflows")
+      ("we" "Checking Email" entry (file+olp+datetree "~/fib/org/Journal.org")
+           "* Checking Email :email:\n\n%?" :clock-in :clock-resume :empty-lines 1)
+
+      ("m" "Metrics Capture")
+      ("mw" "Weight" table-line (file+headline "~/fib/org/Metrics.org" "Weight")
+       "| %U | %^{Weight} | %^{Notes} |" :kill-buffer t)))
+
   )
 
 (use-package org-bullets ;; changes headers so that it doesn't show all of the stars
