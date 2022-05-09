4a133f385cdddb10ca8c858625a626678c83930c
cuberhaus <polcg@hotmail.es>
2022-05-09 18:48:43 +0200

    This is a placeholder

--- .config/chemacs/profiles.el
+++ .config/chemacs/profiles.el
@@ -1,4 +1,5 @@
 (("default" . ((user-emacs-directory . "~/.config/my_emacs")))
  ("doom" . ((user-emacs-directory . "~/.config/doom-emacs") ;; doom emacs must be cloned and install from this directory
            (env . (("DOOMDIR" . "~/.doom.d")))))
+  ("prelude" . ((user-emacs-directory . "~/.config/prelude")))
  )
