b6b80d909aadc37fcb52cef9fd23ab59d83628e9
cuberhaus <polcg@hotmail.es>
2022-05-09 11:46:17 +0200

    works

--- .config/chemacs/profiles.el
+++ .config/chemacs/profiles.el
@@ -1,1 +1,4 @@
-(("default" . ((user-emacs-directory . "~/.config/my_emacs"))))
+(("default" . ((user-emacs-directory . "~/.config/my_emacs")))
+ ("doom" . ((user-emacs-directory . "~/.config/doom-emacs") ;; doom emacs must be cloned and install from this directory
+           (env . (("DOOMDIR" . "~/.doom.d")))))
+ )
