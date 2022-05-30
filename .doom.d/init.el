01a66ca67e1521496dc18bbf758b1850cd3f63aa
Pol Casacuberta <polcg10@gmail.com>
2022-05-30 20:53:55 +0200

    need to configure org roam in doom

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -149,7 +149,8 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org +dragndrop +pandoc +roam2 +pretty)               ; organize your plain life in plain text
+       ;; pretty in org can be expensive, remove if org becomes slow
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
