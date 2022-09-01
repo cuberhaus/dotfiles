1ffeda8b803021d0625defdbc1c78df0eb28a47a
Pol Casacuberta <polcg10@gmail.com>
2022-09-01 16:23:29 +0200

    Added flyspell to doom, cause spelling here was trash ngl

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -83,7 +83,7 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       spell             ; tasing you for misspelling mispelling
+       (spell +aspell +flyspell +everywhere)             ; tasing you for misspelling mispelling
        grammar           ; tasing grammar mistake every you make
 
        :tools
