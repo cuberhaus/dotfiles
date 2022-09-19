4c2924b75ffe8fe07e3a1f3d8e96300d9fbdf1a3
cuberhaus <polcg10@gmail.com>
2022-09-19 22:28:47 +0200

    discord

--- .local/scripts/discord_fix.py
+++ .local/scripts/discord_fix.py
@@ -1,13 +1,14 @@
 import json
 import os
 
-home = os.path.expanduser( '~' )
-path= home  +"/.config/discord/settings.json"
+home = os.path.expanduser('~')
+path = home + "/.config/discord/settings.json"
 
-f = open(path)
-# lines = f.readlines()
-disc = {"SKIP_HOST_UPDATE": True}
-data = json.load(f)
-data.update(disc)
-print(json.dumps(data))
-
+with open(path, 'r+') as f:
+    # lines = f.readlines()
+    disc = {"SKIP_HOST_UPDATE": True}
+    data = json.load(f)
+    data.update(disc)
+    print(json.dumps(data))
+    f.seek(0)
+    json.dump(data, f, indent=4)
