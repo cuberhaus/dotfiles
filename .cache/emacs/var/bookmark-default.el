adec16799a942a85d650a78ac3ae7e9bcd42802b
Pol Casacuberta <polcg10@gmail.com>
2022-12-13 22:15:40 +0100

    This is a placeholder

--- .cache/emacs/var/bookmark-default.el
+++ .cache/emacs/var/bookmark-default.el
@@ -30,6 +30,6 @@
 ("org-capture-last-stored"
  (filename . "~/docs/org/Journal.org")
  (front-context-string . "**** 12:04 AM - ")
- (rear-context-string . "2-11-25 Friday\n\n")
- (position . 11627))
+ (rear-context-string . "-12-13 Tuesday\n\n")
+ (position . 27355))
 )
